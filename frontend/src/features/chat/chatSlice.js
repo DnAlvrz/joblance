15f7646fdd1de6d23ea1897c62dd5c3949297647
Don <mintchip00073@gmail.com>
2022-11-14 13:42:13 +0800

    message user feature finished

--- frontend/src/features/chat/chatSlice.js
+++ frontend/src/features/chat/chatSlice.js
@@ -20,7 +20,6 @@ export const getConversations = createAsyncThunk('chat/getConversations', async(
       error.message || error.toString();
     return thunkAPI.rejectWithValue(message);
   }
-
 });
 
 export const sendUserMessage = createAsyncThunk('chat/sendUserMessage', async (msgData, thunkAPI)=> {
@@ -36,10 +35,22 @@ export const sendUserMessage = createAsyncThunk('chat/sendUserMessage', async (m
 });
 
 
+export const findOrCreateChat = createAsyncThunk('chat/findOrCreateChat', async (receiverId, thunkAPI)=> {
+  try {
+    const userToken = thunkAPI.getState().auth.user.token;
+    return await chatService.findOrCreateChat(userToken,receiverId );
+  } catch (error) {
+    const message = (
+      error.response && error.response.data && error.response.data.message) ||
+      error.message || error.toString();
+    return thunkAPI.rejectWithValue(message);
+  }
+});
+
 export const chatSlice = createSlice({
   name:'chat',
   initialState,
-  reducer: {
+  reducers: {
     reset: (state)=> initialState
   },
   extraReducers: (builder)=> {
@@ -67,9 +78,21 @@ export const chatSlice = createSlice({
         state.isError = true;
         state.message = action.payload;
       })
+      // Get find or new chat
+      .addCase(findOrCreateChat.pending, (state)=> {state.isLoading=true})
+      .addCase(findOrCreateChat.fulfilled, (state, action)=> {
+        state.isLoading=false;
+        state.isError = false;
+        state.conversations.push(action.payload);
+        state.isSuccess=true;
+      })
+      .addCase(findOrCreateChat.rejected, (state, action)=> {
+        state.isLoading=false;
+        state.isError = true;
+        state.message = action.payload;
+      })
   }
 })
-
 
 export const {reset} = chatSlice.actions
 export default chatSlice.reducer
