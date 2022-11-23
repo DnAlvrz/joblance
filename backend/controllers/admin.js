d9622eebe9edf1f037222eff272a321bba9fe0fe
Don <mintchip00073@gmail.com>
2022-11-23 16:43:52 +0800

    modified admin controllers

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -3,40 +3,55 @@ const Job = require('../models/Job');
 const asyncHandler = require('express-async-handler');
 
 const verifyUser = asyncHandler(async(req, res)=> {
-  const userId = req.body.userId
-  if(!userId) {
+  const { userId, status } = req.body
+  if(!userId || !status) {
     res.status(400);
     throw new Error('No user id given');
   }
+
   if(req.user.role.name !== 'admin') {
     res.status(401);
     throw new Error('User unauthorized');
   }
+
+  if(status === 'admin' ) {
+    res.status(401);
+    throw new Error('Changing user status to admin is not allowed.')
+  }
+
   const user = await User.findOne({ _id: userId });
-  if(user.status=== 'verified') {
+
+  if( user.status === 'verified') {
     res.status(400);
     throw new Error('User already verified');
   }
-  user.status = 'verified';
+
+  user.status = status;
   await user.save();
   res.status(200).json({user});
 });
 
 const verifyJob = asyncHandler(async(req, res)=> {
   const jobId = req.body.jobId
+
   if(!jobId) {
     res.status(400);
     throw new Error('No job id given');
   }
+
   if(req.user.role.name !== 'admin') {
+
     res.status(401);
     throw new Error('User unauthorized');
   }
+
   const job = await Job.findOne({ _id: jobId });
+
   if(job.isVeried) {
     res.status(400);
     throw new Error('Job already verified');
   }
+
   job.isVerified = true;
   await job.save();
   res.status(200).json({job});
@@ -48,7 +63,7 @@ const getUnverifiedUsers = asyncHandler(async(req,res)=> {
     throw new Error('User unauthorized');
   }
 
-  const unverfiedUsers = await User.find({ status: 'unverfied' }, {
+  const unverfiedUsers = await User.find({ status: 'unverified' }, {
     password: 0,
     __v: 0,
   });
